ade4b1fd71a8d8e7a5caec52765dd82b480b45e7
jonames <gyjhkx57164@027168.com>
2017-07-24 05:59:01 +0000

    1st commit

---
+++ src/pages/movie.vue
@@ -0,0 +1,14 @@
+<template>
+	<p class="declare">开发中..</p>
+</template>
+<script>
+export default {
+	name: "header"
+}
+</script>
+<style>
+.declare{
+    padding: .4rem;
+	font-size: .3rem;
+}
+</style>
